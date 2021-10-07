13798edd0021e1f434d0887450e6b981d70ed935
kotahorii <khorii.micron@gmail.com>
2021-10-07 14:04:22 +0000

    [Update] routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'users/show'
-  get 'users/edit'
+  # get 'users/show'
+  # get 'users/edit'
   devise_for :users
+  resources :books, only: [:index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
 
 end
