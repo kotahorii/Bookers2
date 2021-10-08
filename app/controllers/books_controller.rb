d5056855b457d1cf75a6c6a0057a0c4539e838f1
kotahorii <khorii.micron@gmail.com>
2021-10-08 13:33:31 +0000

    [Add] create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,17 @@ class BooksController < ApplicationController
     @book = Book.new
     @user = User.find(current_user.id)
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(current_user.id)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'users/show'
   # get 'users/edit'
   devise_for :users
-  resources :books, only: [:index, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
 
 end
