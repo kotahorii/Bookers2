5a362faf4eb69383ece76657e9b2cf6df3632aef
kotahorii <khorii.micron@gmail.com>
2021-10-08 15:14:55 +0000

    [Add] users/show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+    @users = User.all
+    @user = User.find(current_user.id)
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books.all
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # get 'users/edit'
   devise_for :users
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :create, :show, :edit, :update]
 
 end
