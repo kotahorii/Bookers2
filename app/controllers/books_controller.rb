98463aeba8bd5d9f7dd9f2f2754c1cb98d879ff7
kotahorii <khorii.micron@gmail.com>
2021-10-09 15:13:44 +0000

    [Add] update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,8 +18,15 @@ class BooksController < ApplicationController
     @user = User.find(@book.user_id)
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def update
-    
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+
   end
 
   private
