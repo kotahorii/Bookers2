e14d26e9f6e58f0ffb96a957fe2a7db233d362a3
kotahorii <khorii.micron@gmail.com>
2021-10-09 16:25:53 +0000

    [Add] user update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8f0882fed65492273ed5b68e83d4ed76c6738e296c70bd9926b5f3ed1f1853f63709164bae2897e87b1f49202b74ba01be16545d4b6198cd707ef3a6ba6e57cf'
